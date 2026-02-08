9fd311ace16d5d745d16baddad44f80f1b47f884
Hayden Ho <z5589170@ad.unsw.edu.au>
2026-02-08 14:52:14 +1100

    set up supabase database and began work on journal page

--- src/pages/JournalPage.jsx
+++ src/pages/JournalPage.jsx
@@ -1,10 +1,11 @@
+import TextareaAutosize from 'react-textarea-autosize';
 
 function JournalPage() {
   return (
-    <>
-      sdhbchdasbhjcadsjb
-    </>
+    <div className="flex justify-center" >
+      <TextareaAutosize className="w-150 border-2 border-solid" minRows={5} defaultValue="dear journal" />
+    </div>
   );
 }
 
-export default JournalPage;
+export default JournalPage; 
